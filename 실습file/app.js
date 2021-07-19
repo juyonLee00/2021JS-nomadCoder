80f58b2bdbc57b25a0d0a24a07c56c0e57e7d77c
LemonYellowParrot <ljy012900@gmail.com>
2021-07-19 17:41:23 +0900

    input values 실습

--- 실습file/app.js
+++ 실습file/app.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const loginform = document.getElementById("login-form");
+const loginInput = loginform.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function onLoginBtnClick(){
+console.dir(loginInput.dir);
+
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
