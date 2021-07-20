ca2d4fe0bf5faa7a0bee611fcadf8d3edd834b80
LemonYellowParrot <ljy012900@gmail.com>
2021-07-20 23:47:41 +0900

    login 구현

--- 실습file/app.js
+++ 실습file/app.js
@@ -1,10 +1,29 @@
-const loginform = document.getElementById("login-form");
-const loginInput = loginform.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = document.querySelector("#login-form Input");
+const loginButton = document.querySelector("#login-form Input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick(){
-console.dir(loginInput.dir);
+ const HIDDEN_CLASSNAME = "hidden";
+ const USERNAME_KEY = "username";
 
-}
+ function onLoginSubmit(event) {
+   event.preventDefault();
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   const username = loginInput.value;
+   localStorage.setItem(USERNAME_KEY, username);
+   paintGreetings(username);
+ }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+ function paintGreetings(username) {
+   greeting.innerText = `Hello ${username}`;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+ }
+
+ const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+ if (savedUsername === null) {
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit", onLoginSubmit);
+ } else {
+   paintGreetings(savedUsername);
+ }
